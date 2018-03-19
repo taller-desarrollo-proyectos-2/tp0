7e3b5126be7c5667188a39b0914b12f5f583660a
fernandonitz <nitzfernando@gmail.com>
2018-03-19 02:18:08 -0300

    agrego la posisicion de la ciudad
    ademas, cambios en la pantalla principal

--- app/src/main/java/com/fiuba/taller/tp0/SelectCityActivity.java
+++ app/src/main/java/com/fiuba/taller/tp0/SelectCityActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,7 +20,7 @@ import com.fiuba.taller.tp0.services.weather.WeatherService;
 public class SelectCityActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cities_main);
 
@@ -27,5 +28,17 @@ public class SelectCityActivity extends AppCompatActivity {
         final ListView milistacities = (ListView)findViewById(R.id.milistacities);
         ArrayAdapter<String> adapterCity = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, s.getCities());
         milistacities.setAdapter(adapterCity);
+
+
+        milistacities.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> list, View v, int pos, long id) {
+                saveCityPreference(pos);
+                finish();
+            }
+        });
+    }
+    public void saveCityPreference(int pos){
+        CityPreference cityPrefs = new CityPreference(this);
+        cityPrefs.setPreferedCityIndexKey(pos);
     }
 }
